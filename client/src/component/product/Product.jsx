4802f155884f2f7b02a785b459e7c58f65eae91a
imhaeunk <imhaeunk@gmail.com>
2021-12-22 15:37:30 +0900

    Feat: add Product list by NAVER API call
    -

--- client/src/component/product/Product.jsx
+++ client/src/component/product/Product.jsx
@@ -1,11 +1,92 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react"
+import { getProducts, addToCart } from "../../apiCalls"
+import {
+  Button,
+  Col,
+  Container,
+  Input,
+  InputGroup,
+  ListGroup,
+  ListGroupItem,
+  Row,
+  Table,
+} from "reactstrap"
 const Product = (props) => {
-  return (
-    <div>
-      <h2>상품</h2>
-    </div>
-  );
-};
+  const [productList, setProductList] = useState([])
 
-export default Product;
+  useEffect(() => {
+    ;(async () => {
+      const data = await getProducts()
+      console.log(data)
+      setProductList(data)
+    })()
+  }, [])
+
+  return (
+    <Container>
+      <h1>상품</h1>
+      <Table
+        style={{
+          textAlign: "center",
+          verticalAlign: "middle",
+        }}
+      >
+        <thead>
+          <tr>
+            <th>카테고리</th>
+            <th>상품</th>
+            <th style={{ width: "40%" }}>상품명</th>
+            <th>가격</th>
+            <th>담기</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productList.map((product) => (
+            <tr key={product.product_id}>
+              <td>
+                <span>
+                  {product.category1}
+                  <br />
+                  {product.category2}
+                  <br />
+                  {product.category3}
+                  <br />
+                  {product.category4}
+                </span>
+              </td>
+              <td>
+                <img
+                  alt="상품썸네일"
+                  src={product.image}
+                  style={{ width: 100 }}
+                />
+              </td>
+              <td>
+                <a
+                  style={{ margin: 0, fontSize: "inherit" }}
+                  href={product.link}
+                >
+                  {product.title}
+                </a>
+              </td>
+              <td>
+                <span>{Number(product.l_price).toLocaleString()}</span>
+              </td>
+              <td>
+                <Button
+                  onClick={() => {
+                    addToCart(product.product_id)
+                  }}
+                >
+                  담기
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  )
+}
+
+export default Product
